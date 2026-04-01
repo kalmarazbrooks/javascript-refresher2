3341ba460dfde5a8cae934308e2d14ee28423960
Kaden Almaraz <kalmaraz@brooksschool.org>
2026-04-01 12:01:21 -0400

    Challenge 2

--- events/myScript.js
+++ events/myScript.js
@@ -12,3 +12,25 @@ function dontYouWish() {
     message.innerHTML = `Hello ${nameInput.value}. Don't you wish you were a ${animalInput.value.toLowerCase()}?`
 }
 
+// Challenge 2
+
+button.addEventListener('mouseover', changeColor)
+button.addEventListener('mouseleave', revertColor)
+
+function changeColor() {
+    nameInput.style.backgroundColor = getRandomColor()
+}
+function revertColor() {
+    nameInput.style.backgroundColor = '#FFFFFF'
+}
+
+function getRandomColor() { // http://stackoverflow.com/questions/1484506/random-color-generator
+    const code = '0123456789ABCDEF'
+    let color = '#'
+
+    for (let i = 0; i < 6; i++) {
+        color += code[Math.floor(Math.random() * 16)]
+    }
+
+    return color
+}
